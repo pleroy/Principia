1c6db86ce50172351902e47e1a1af19f172bae65
pleroy <phl+personal@google.com>
2014-12-15 22:35:27 +0100

    Suppress logging.

--- testing_utilities/heap_checked_test.hpp
+++ testing_utilities/heap_checked_test.hpp
@@ -1,10 +1,16 @@
 #include "base/heap_checker.hpp"
+#include "glog/logging.h"
 #include "gtest/gtest.h"
 
 namespace principia {
 namespace testing_utilities {
 
 class HeapCheckedTest : public testing::Test {
+protected:
+  HeapCheckedTest() : testing::Test() {
+    FLAGS_minloglevel = google::FATAL;
+  }
+  
  private:
   base::HeapChecker heap_checker_;
 };
