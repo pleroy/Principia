4de2402dd0536b3f6573141443c1aa48c5a992b5
pleroy <pascal.h.leroy@gmail.com>
2020-10-22 21:38:45 +0200

    A test that does not terminate.

--- numerics/quadrature_test.cpp
+++ numerics/quadrature_test.cpp
@@ -3,9 +3,14 @@
 
 #include <limits>
 
+#include "geometry/frame.hpp"
+#include "geometry/named_quantities.hpp"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "numerics/polynomial.hpp"
+#include "numerics/polynomial_evaluators.hpp"
 #include "quantities/elementary_functions.hpp"
+#include "quantities/si.hpp"
 #include "testing_utilities/almost_equals.hpp"
 #include "testing_utilities/is_near.hpp"
 #include "testing_utilities/numerics_matchers.hpp"
@@ -14,10 +19,18 @@ namespace principia {
 namespace numerics {
 namespace quadrature {
 
+using geometry::Displacement;
+using geometry::Frame;
+using geometry::Handedness;
+using geometry::Inertial;
 using quantities::Angle;
 using quantities::Cos;
+using quantities::Pow;
 using quantities::Sin;
+using quantities::Time;
+using quantities::si::Metre;
 using quantities::si::Radian;
+using quantities::si::Second;
 using testing_utilities::AlmostEquals;
 using testing_utilities::IsNear;
 using testing_utilities::RelativeErrorFrom;
@@ -25,11 +38,17 @@ using testing_utilities::operator""_⑴;
 using ::testing::AnyOf;
 using ::testing::Eq;
 
-class QuadratureTest : public ::testing::Test {};
+class QuadratureTest : public ::testing::Test {
+ protected:
+  using World = Frame<serialization::Frame::TestTag,
+                      Inertial,
+                      Handedness::Right,
+                      serialization::Frame::TEST>;
+};
 
 TEST_F(QuadratureTest, Sin) {
   int evaluations = 0;
-  auto const f = [&evaluations](Angle const x) {
+  auto const f = [&evaluations](Angle const& x) {
     ++evaluations;
     return Sin(x);
   };
@@ -77,7 +96,7 @@ TEST_F(QuadratureTest, Sin) {
 }
 
 TEST_F(QuadratureTest, Sin2) {
-  auto const f = [](Angle const x) { return Sin(2 * x); };
+  auto const f = [](Angle const& x) { return Sin(2 * x); };
   auto const ʃf = (Cos(4 * Radian) - Cos(10 * Radian)) / 2 * Radian;
   EXPECT_THAT(GaussLegendre<1>(f, -2.0 * Radian, 5.0 * Radian),
               RelativeErrorFrom(ʃf, IsNear(9.7_⑴)));
@@ -115,7 +134,7 @@ TEST_F(QuadratureTest, Sin2) {
 
 TEST_F(QuadratureTest, Sin10) {
   int evaluations = 0;
-  auto const f = [&evaluations](Angle const x) {
+  auto const f = [&evaluations](Angle const& x) {
     ++evaluations;
     return Sin(10 * x);
   };
@@ -129,6 +148,55 @@ TEST_F(QuadratureTest, Sin10) {
               AlmostEquals(ʃf, 2, 20));
   EXPECT_THAT(evaluations, AnyOf(Eq(32769), Eq(65537), Eq(262145), Eq(524289)));
 }
+
+#if 0
+TEST_F(QuadratureTest, NonTerminating) {
+  int evaluations = 0;
+  auto const left =
+      PolynomialInMonomialBasis<Displacement<World>, Time, 3, EstrinEvaluator>(
+          {Displacement<World>({-2.64575131106458450e+00 * Metre,
+                                +0.00000000000000000e+00 * Metre,
+                                +0.00000000000000000e+00 * Metre}),
+           Displacement<World>({+4.02334445113228672e-06 * Metre,
+                                +0.00000000000000000e+00 * Metre,
+                                +0.00000000000000000e+00 * Metre}) /
+               Second,
+           Displacement<World>({-1.27463011048138714e-12 * Metre,
+                                +0.00000000000000000e+00 * Metre,
+                                +0.00000000000000000e+00 * Metre}) /
+               Pow<2>(Second),
+           Displacement<World>({+1.07683673837640856e-19 * Metre,
+                                +0.00000000000000000e+00 * Metre,
+                                +0.00000000000000000e+00 * Metre}) /
+               Pow<3>(Second)});
+  auto const right =
+      PolynomialInMonomialBasis<Displacement<World>, Time, 2, EstrinEvaluator>(
+          {Displacement<World>({-3.40821468615081086e-02 * Metre,
+                                +0.00000000000000000e+00 * Metre,
+                                +0.00000000000000000e+00 * Metre}),
+           Displacement<World>({+2.05092765462352891e-08 * Metre,
+                                +0.00000000000000000e+00 * Metre,
+                                +0.00000000000000000e+00 * Metre}) /
+               Second,
+           Displacement<World>({-2.50948167739304229e-15 * Metre,
+                                +0.00000000000000000e+00 * Metre,
+                                +0.00000000000000000e+00 * Metre}) /
+               Pow<2>(Second)});
+
+  auto const f = [&evaluations, &left, &right](Time const& t) {
+    ++evaluations;
+    return InnerProduct(left(t), right(t));
+  };
+  EXPECT_THAT(AutomaticClenshawCurtis(
+                  f,
+                  0 * Second,
+                  +7.89120000000000000e+06 * Second,
+                  /*max_relative_error=*/0x1p-32,
+                  /*max_points=*/std::nullopt),
+              AlmostEquals(0 * Pow<2>(Metre) * Second, 0));
+  EXPECT_THAT(evaluations, AnyOf(Eq(666)));
+}
+#endif
 
 }  // namespace quadrature
 }  // namespace numerics
