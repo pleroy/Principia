780704c931b9be09287326b20d23180e52ec9cff
Pascal Leroy <pascal.h.leroy@gmail.com>
2021-01-23 14:26:39 +0100

    A new failing test.

--- geometry/symmetric_bilinear_form_test.cpp
+++ geometry/symmetric_bilinear_form_test.cpp
@@ -286,6 +286,7 @@ TEST_F(SymmetricBilinearFormTest, Serialization) {
 }
 
 TEST_F(SymmetricBilinearFormTest, Diagonalize) {
+#if 0
   // A simple test where it's clear what the eigensystem should be.
   {
     auto const f = MakeSymmetricBilinearForm<World>(
@@ -548,6 +549,31 @@ TEST_F(SymmetricBilinearFormTest, Diagonalize) {
                             {+0.00000000000000000e+00,
                              +8.32667268468867405e-17,
                              +4.00000065565109519e+00}}));
+    auto const f_eigensystem = f.Diagonalize<Eigenworld>();
+
+    EXPECT_THAT(f_eigensystem.rotation.quaternion().Norm(),
+                AlmostEquals(1.0, 0))
+        << f << "\n" << f_eigensystem.form;
+    Vector<double, Eigenworld> const e₀({1, 0, 0});
+    Vector<double, Eigenworld> const e₁({0, 1, 0});
+    Vector<double, Eigenworld> const e₂({0, 0, 1});
+
+    EXPECT_THAT(f_eigensystem.rotation(e₀), Componentwise(1, 0, 0));
+    EXPECT_THAT(f_eigensystem.rotation(e₁), Componentwise(0, 1, 0));
+    EXPECT_THAT(f_eigensystem.rotation(e₂), Componentwise(0, 0, 1));
+  }
+#endif
+  {
+    auto const f = MakeSymmetricBilinearForm<World>(
+        R3x3Matrix<double>({{+3.99999646469950676e+00,
+                             +0.00000000000000000e+00,
+                             +0.00000000000000000e+00},
+                            {+0.00000000000000000e+00,
+                             +3.99999646469950720e+00,
+                             +0.00000000000000000e+00},
+                            {+0.00000000000000000e+00,
+                             +0.00000000000000000e+00,
+                             +3.99999646469950676e+00}}));
     auto const f_eigensystem = f.Diagonalize<Eigenworld>();
 
     EXPECT_THAT(f_eigensystem.rotation.quaternion().Norm(),
